ffa7a070af8942c5117eb926ae258a16d01943db
joaowillamy <willamyse@gmail.com>
2019-11-08 15:19:41 -0300

    feat(customer): aliases

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
 
+  it '#full_name - Sobrescrevendo Atributo' do
+    customer = create(:user, name: "João Willamy" )
+    customer1 = create(:customer, name: "João Willamy" )
+    expect(customer.full_name).to eq("Sr. João Willamy")
+    expect(customer1.full_name).to eq("Sr. João Willamy")
+  end
+
   it '#full_name' do
     customer = create(:customer)
     expect(customer.name).to be_a(String)
