6179c5b87b4d0bd21a2a86895255c3a96ccb12d7
joaowillamy <willamyse@gmail.com>
2019-11-08 15:33:08 -0300

    feat(customer): Herança

--- test_app/db/schema.rb
+++ test_app/db/schema.rb
@@ -15,11 +15,10 @@ ActiveRecord::Schema.define(version: 2019_11_08_173022) do
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "testes", force: :cascade do |t|
+    t.boolean "vip"
+    t.integer "days_to_pay"
+    t.string "gender"
+    t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
