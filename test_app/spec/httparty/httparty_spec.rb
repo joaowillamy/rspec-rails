c4f95e64f1ef6a17bb166548b5628aba6c537b06
joaowillamy <willamyse@gmail.com>
2019-11-09 13:21:42 -0300

    feature(http): save request with vcr

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,7 +1,8 @@
 require 'httparty'
+# require 'vcr'
 
 describe 'HTTParty' do
-  it 'content-type' do
+  it 'content-type stubbing' do
     stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2")
       .to_return(status: 200, body: "", headers: { 'content-type': 'application/json'})
 
@@ -9,4 +10,25 @@ describe 'HTTParty' do
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
   end
+
+  it 'content-type vcr' do
+    VCR.use_cassette('jsonplaceholder/posts') do
+      response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+      content_type = response.headers['content-type']
+      expect(content_type).to match(/application\/json/)
+     end
+  end
+  
+  it 'vcr metadata- ignore param id',  vcr: { cassette_name: 'jsonplaceholder/posts',  match_requests_on: [:body] }  do
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)
+  end
+
+  it 'vcr - record modes',  vcr: { cassette_name: 'jsonplaceholder/posts', :record => :new_episodes }  do
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)
+  end
+
 end
