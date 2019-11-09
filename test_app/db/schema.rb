d9b269e7bef2c45c67eb0a81984fabc38534b2a7
joaowillamy <willamyse@gmail.com>
2019-11-09 09:36:00 -0300

    feat(order): add new module

--- test_app/db/schema.rb
+++ test_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_173022) do
+ActiveRecord::Schema.define(version: 2019_11_09_121001) do
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
@@ -23,4 +23,13 @@ ActiveRecord::Schema.define(version: 2019_11_08_173022) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.string "description"
+    t.integer "customer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
+  add_foreign_key "orders", "customers"
 end
